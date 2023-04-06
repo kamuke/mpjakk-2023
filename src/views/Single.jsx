41167b900cc0ffdec5738f1bd21bdede599d7864
Kerttu <80481715+kamuke@users.noreply.github.com>
2023-04-06 12:11:31 +0300

    filters

--- src/views/Single.jsx
+++ src/views/Single.jsx
@@ -1,22 +1,48 @@
-import {Card, CardMedia, Typography} from '@mui/material';
+import {Card, CardMedia, CardContent, Typography} from '@mui/material';
 import {useLocation} from 'react-router-dom';
 import {mediaUrl} from '../utils/variables';
 
 const Single = () => {
   const {state} = useLocation();
   const file = state.file;
+  let allData = {
+    desc: file.description,
+    filters: {
+      brightness: 100,
+      contrast: 100,
+      saturation: 100,
+    },
+  };
+
+  try {
+    allData = JSON.parse(file.description);
+  } catch (error) {
+    console.log(error.message);
+  }
 
   return (
     <>
       <Typography component="h1" variant="h3">
         {file.title}
       </Typography>
-      <Card>
+      <Card title={file.title}>
         <CardMedia
           component={'img'}
           src={mediaUrl + file.filename}
           title={file.title}
+          style={{
+            width: 600,
+            height: 400,
+            filter: `
+            brightness(${allData.filters.brightness}%)
+            contrast(${allData.filters.contrast}%)
+            saturate(${allData.filters.saturation}%)
+            `,
+          }}
         />
+        <CardContent>
+          <Typography variant="body1">{allData.desc}</Typography>
+        </CardContent>
       </Card>
     </>
   );
--- src/views/Upload.jsx
+++ src/views/Upload.jsx
@@ -1,27 +1,127 @@
 /* eslint-disable no-unused-vars */
-import {Box, Button} from '@mui/material';
+import {Box, Button, Slider, Stack} from '@mui/material';
 // import PropTypes from 'prop-types';
 import useForm from './../hooks/FormHooks';
+import {useState} from 'react';
+import {useMedia, useTag} from '../hooks/ApiHooks';
+import {useNavigate} from 'react-router-dom';
+import {appId} from '../utils/variables';
 
 const Upload = (props) => {
+  const [file, setFile] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(
+    'https://placekitten.com/600/400'
+  );
+  // 'https://placehold.co/600x400?text=Choose-media'
+  const {postMedia} = useMedia();
+  const {postTag} = useTag();
+  const navigate = useNavigate();
+
   const initValues = {
     tilte: '',
     description: '',
-    file: '',
   };
 
-  const doUpload = () => {};
+  const filterInitValues = {
+    brightness: 100,
+    constrast: 100,
+    saturation: 100,
+  };
+
+  const doUpload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('title', inputs.title);
+      const allData = {
+        desc: inputs.description,
+        filters: filterInputs,
+      };
+      formData.append('description', JSON.stringify(allData));
+      formData.append('file', file);
+      const userToken = localStorage.getItem('userToken');
+      // check if file
+      const uploadResult = await postMedia(formData, userToken);
+      const tagResult = await postTag(
+        {
+          file_id: uploadResult.file_id,
+          tag: appId,
+        },
+        userToken
+      );
+      navigate('/home');
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const handleFileChange = (event) => {
+    event.persist();
+    setFile(event.target.files[0]);
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      setSelectedImage(reader.result);
+    });
+    reader.readAsDataURL(file);
+  };
 
   const {inputs, handleSubmit, handleInputChange} = useForm(
     doUpload,
     initValues
   );
 
-  console.log('upload', inputs);
+  const {inputs: filterInputs, handleInputChange: handleFilterChange} = useForm(
+    null,
+    filterInitValues
+  );
 
   return (
     <Box>
-      <form>
+      <img
+        src={selectedImage}
+        alt="Preview of the uploaded media"
+        style={{
+          width: 600,
+          height: 400,
+          filter: `
+          brightness(${filterInputs.brightness}%)
+          contrast(${filterInputs.contrast}%)
+          saturate(${filterInputs.saturation}%)
+          `,
+        }}
+      />
+      {/* <Stack></Stack> */}
+      <Slider
+        name="brightness"
+        defaultValue={100}
+        min={0}
+        max={200}
+        step={1}
+        valueLabelDisplay="auto"
+        onChange={handleFilterChange}
+        value={filterInputs.brightness}
+      />
+      <Slider
+        name="contrast"
+        defaultValue={100}
+        min={0}
+        max={200}
+        step={1}
+        valueLabelDisplay="auto"
+        onChange={handleFilterChange}
+        value={filterInputs.contrast}
+      />
+      <Slider
+        name="saturation"
+        defaultValue={100}
+        min={0}
+        max={200}
+        step={1}
+        valueLabelDisplay="auto"
+        onChange={handleFilterChange}
+        value={filterInputs.saturation}
+      />
+      {/** TODO: Validation (no file) and styling */}
+      <form onSubmit={handleSubmit}>
         <input
           onChange={handleInputChange}
           type="text"
@@ -36,10 +136,10 @@ const Upload = (props) => {
           value={inputs.description}
         ></textarea>
         <input
-          onChange={handleInputChange}
+          onChange={handleFileChange}
           type="file"
           name="file"
-          accept="image/* video/* audio/*"
+          accept="image/*,video/*,audio/*"
         ></input>
         <Button type="submit">Upload media</Button>
       </form>
